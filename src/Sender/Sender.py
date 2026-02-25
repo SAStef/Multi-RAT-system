80482f1eef713a1aa02482af252a37580e1c8ec0
VedantaSinha <sinha.vedanta@gmail.com>
2026-02-25 15:37:49 +0100

    Update Sender.py

--- src/Sender/Sender.py
+++ src/Sender/Sender.py
@@ -1,14 +1,14 @@
-import socket as s
+import socket as s  
 import os
 sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
 
-ip = "192.38.81.6"
+ip = "10.209.154.26"
 port = 6967
 
 a = 0
 while a<8:
-    message = os.urandom(8)
-    sock.sendto(message.encode(), (ip, port))
+    message1 = (os.urandom(8))
+    sock.sendto(message1, (ip, port))
     a+=1
 
 
