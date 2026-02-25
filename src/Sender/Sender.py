0f4732473099c9e1754f5a34c7f8ccc040f0448e
VedantaSinha <sinha.vedanta@gmail.com>
2026-02-25 15:04:50 +0100

    Update Sender.py

--- src/Sender/Sender.py
+++ src/Sender/Sender.py
@@ -1,1 +1,30 @@
 import socket as s
+sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
+
+ip = "192.38.81.6"
+port = 6967
+message = "Hello World"
+sock.sendto(message.encode(), (ip, port))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sock.close()
